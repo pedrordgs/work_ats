69670c9b47161662532fd4d4a0b85683cb4e599c
PedroSilva9 <ojosepedro@gmail.com>
2020-12-09 14:15:26 +0000

    added gitgnore and refactored the code, sequence . map = mapM

--- ats.hs
+++ ats.hs
@@ -12,30 +12,31 @@ import System.IO
 import System.Process
 import Cp
 import Iso
+import Control.Applicative
 
 type Package = String
 
 format :: FilePath -> IO [FilePath]
-format d = fmap (map (d ++) . flip (\\) [".",".."]) $ getDirectoryContents d
+format d = map (d ++) . flip (\\) [".",".."] <$> getDirectoryContents d
 
 func :: FilePath -> IO [FilePath]
 func d = do (a,b) <- partitionM doesFileExist =<< format d
-            let (aa, bb) = (filter filterFunc a, map (\x -> x ++ "/") b)
+            let (aa, bb) = (filter filterFunc a, map (++ "/") b)
             case bb of [] -> return aa
-                       x -> func_aux (aa,bb)
+                       x -> funcAux (aa,bb)
     where
         filterFunc x = (not . isPrefixOf "."  . awful $ x) && ("java" `isSuffixOf` x)
         awful = reverse >=< takeWhile (/= '/')
 
-func_aux :: ([FilePath], [FilePath]) -> IO [FilePath]
-func_aux (a,b) = do l <- sequence $ map func b
-                    return $ a ++ concat l
+funcAux :: ([FilePath], [FilePath]) -> IO [FilePath]
+funcAux (a,b) = do l <- mapM func b
+                   return $ a ++ concat l
 
-foo :: IO [([Char], [FilePath])]
-foo = sequence . map (dstr . split fullPath func) =<< dirFilesFullPath
+foo :: IO [(String, [FilePath])]
+foo = mapM (dstr . split fullPath func) =<< dirFilesFullPath
     where strip = (++ "/src/main/java/") . reverse >=< takeWhile (/= '/') . tail
-          fullPath s = fmap (++ "/projects_maven/") getCurrentDirectory >>= pure . (++ (strip s))
-          dirFilesFullPath = getCurrentDirectory >>= pure . (++ "/projectsPOO_1920/") >>= format >>= return . map (snoc "/")
+          fullPath s = (++ strip s) <$> fmap (++ "/projects_maven/") getCurrentDirectory
+          dirFilesFullPath = map (snoc "/") <$> ((++ "/projectsPOO_1920/") <$> getCurrentDirectory >>= format)
           -- dirFilesFullPath = getCurrentDirectory >>= pure . (++ "/mini_projects/") >>= format >>= return . map (snoc "/")
           snoc a s = s ++ a
 
@@ -44,22 +45,21 @@ getPkg :: FilePath -> IO Package
 getPkg = readFileOP >=> return . cutMaybe . helper . splitOn "\n"
     where cutMaybe = cond isNothing nil fromJust
           readFileOP = flip openFile ReadMode >=> (\h -> do {hSetEncoding h latin1; hGetContents h})
-          replace a b l = map (\x -> if x == a then b else x) l
+          replace a b = map (\x -> if x == a then b else x)
           helper = find (isPrefixOf "package") >=> return . replace '.' '/' . tail . dropWhile (/= ' ') . (reverse >=< tail . dropWhile (/= ';'))
 
 pkgSort :: [FilePath] -> IO [(Package, [FilePath])]
-pkgSort =  fmap (map (split (p1 . head) (map p2)) . groupBy factorG . sortBy factorS) . sequence . map (rstr . split getPkg id)
+pkgSort =  fmap (map (split (p1 . head) (map p2)) . groupBy factorG . sortBy factorS) . mapM (rstr . split getPkg id)
     where factorS x y = compare (p1 x) (p1 y)
           factorG x y = p1 x == p1 y
 
 
 f (x, l) = concatMap (\(p, lf) -> (if p /= "" then "mkdir -p " ++ x ++ p else ""):map (\fi -> "cp \"" ++ fi ++ "\" " ++ x ++ p) lf) l
 
-allCommands = fmap (concatMap ((uncurry (:)) . split ((++) "mkdir -p " . p1) f)) $ commands
+allCommands = fmap (concatMap (uncurry (:) . split ((++) "mkdir -p " . p1) f)) commands
 
-runCommands = allCommands >>= sequence . map callCommand
+runCommands = allCommands >>= mapM callCommand
 
-commands = foo >>= sequence . map (lstr . (id >< pkgSort))
+commands = foo >>= mapM (lstr . (id >< pkgSort))
 
-main = do
-  allCommands >>= mapM_ putStrLn
+main = allCommands >>= mapM_ putStrLn
