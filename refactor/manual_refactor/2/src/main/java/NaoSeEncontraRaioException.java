acec9ab5397cd5397086aec7a792ae32196a3639
pedrordgs <pedrombr99@gmail.com>
2021-01-24 16:30:47 +0000

    doing refactoring

---
+++ refactor/manual_refactor/2/src/main/java/NaoSeEncontraRaioException.java
@@ -0,0 +1,13 @@
+
+/**
+ * Escreva a descrição da classe NaoSeEncontraRaioException aqui.
+ * 
+ * @author (seu nome) 
+ * @version (número de versão ou data)
+ */
+public class NaoSeEncontraRaioException extends Exception
+{
+    public NaoSeEncontraRaioException() {
+        super();
+    }
+}
