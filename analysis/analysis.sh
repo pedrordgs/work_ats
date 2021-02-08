c33a8b61e63c893406e86891d8bbe09ef73bf48a
pedrordgs <pedrombr99@gmail.com>
2021-02-08 12:33:21 +0000

    added presentation

--- analysis/analysis.sh
+++ analysis/analysis.sh
@@ -1,47 +1,47 @@
 #!/bin/bash
 
-# mkdir -p ../results/graphs
+mkdir -p ../results/graphs
 
-# echo "==============================" > ../results/metrics_analysis
-# echo "======= NO REFACTORING =======" >> ../results/metrics_analysis
-# echo "==============================" >> ../results/metrics_analysis
+echo "==============================" > ../results/metrics_analysis
+echo "======= NO REFACTORING =======" >> ../results/metrics_analysis
+echo "==============================" >> ../results/metrics_analysis
 
-# Rscript script.r ../results/sonarqube.csv ../results/graphs >> ../results/metrics_analysis
+Rscript script.r ../results/sonarqube.csv ../results/graphs >> ../results/metrics_analysis
 
-# if [ $# -ne 0 ] && [ $1 == "refactor" ]; then
-#   mkdir -p ../results/graphs_refactor
+if [ $# -ne 0 ] && [ $1 == "refactor" ]; then
+  mkdir -p ../results/graphs_refactor
 
-#   echo "=============================" >> ../results/metrics_analysis
-#   echo "======== REFACTORING ========" >> ../results/metrics_analysis
-#   echo "=============================" >> ../results/metrics_analysis
-#   Rscript script.r ../results/sonarqube_refactor.csv ../results/graphs_refactor >> ../results/metrics_analysis
-# else
-#   echo "Skipping auto refactor analyze"
-# fi
+  echo "=============================" >> ../results/metrics_analysis
+  echo "======== REFACTORING ========" >> ../results/metrics_analysis
+  echo "=============================" >> ../results/metrics_analysis
+  Rscript script.r ../results/sonarqube_refactor.csv ../results/graphs_refactor >> ../results/metrics_analysis
+else
+  echo "Skipping auto refactor analyze"
+fi
 
-# mkdir projects
+mkdir projects
 
-# cp -r ../projects_maven/67 projects
-# cp -r ../projects_maven/83 projects
-# cp -r ../projects_maven/2 projects
+cp -r ../projects_maven/67 projects
+cp -r ../projects_maven/83 projects
+cp -r ../projects_maven/2 projects
 
-# cp pom.xml projects/67
-# cp pom.xml projects/83
-# cp pom.xml projects/2
+cp pom.xml projects/67
+cp pom.xml projects/83
+cp pom.xml projects/2
 
-# ../helpers/javaswitcher.sh 8
+../helpers/javaswitcher.sh 8
 
-# for p in projects/*; do
-#   cd $p
-#   PROJECT=$(echo $p | awk -F '/' {'print $2'})
-#   CLASS=$(grep -RH "void main[^a-zA-Z]" * | awk -F ":" {'print $1'} | sed 's/src\/main\/java\///;s/\.java//;s/\//./g')
-#   sed -i "s/PROJECT_ID/$PROJECT/;s/CLASSE/$CLASS/" pom.xml 2> /dev/null
-#   mvn -DmemoryInMB=2000 -Dcores=4 evosuite:generate evosuite:export surefire-report:report
-#   mvn cobertura:cobertura
-#   mkdir -p ../../../results/tests/$PROJECT
-#   cp -r target/site/* ../../../results/tests/$PROJECT
-#   cd ../..
-# done
+for p in projects/*; do
+  cd $p
+  PROJECT=$(echo $p | awk -F '/' {'print $2'})
+  CLASS=$(grep -RH "void main[^a-zA-Z]" * | awk -F ":" {'print $1'} | sed 's/src\/main\/java\///;s/\.java//;s/\//./g')
+  sed -i "s/PROJECT_ID/$PROJECT/;s/CLASSE/$CLASS/" pom.xml 2> /dev/null
+  mvn -DmemoryInMB=2000 -Dcores=4 evosuite:generate evosuite:export surefire-report:report
+  mvn cobertura:cobertura
+  mkdir -p ../../../results/tests/$PROJECT
+  cp -r target/site/* ../../../results/tests/$PROJECT
+  cd ../..
+done
 
 ../helpers/javaswitcher.sh 11
 
